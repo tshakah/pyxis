1433e95abac8996965c9efc88bf32e5a2584ca3d
Gsciarra <giampiero.sciarra@prima.it>
2021-12-16 10:51:30 +0100

    [PRIMA-11997]: [Pyxis-scss] Implementazione delle icone (#16)

--- packages/pyxis-react/src/components/Cose/index.ts
+++
@@ -1,2 +0,0 @@
-export { default } from './Cose';
-export type { CoseProps } from './Cose';
--- packages/pyxis-react/src/index.ts
+++ packages/pyxis-react/src/index.ts
@@ -1,3 +1,2 @@
 // eslint-disable-next-line import/prefer-default-export
 export { default as Button } from './components/Button';
-export { default as Cose } from './components/Cose';
