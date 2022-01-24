f3a85424d838cc5db1ecbbb9c53f3c25f8018bdd
Alessio Santangelo <alessio.santangelo@prima.it>
2022-01-24 12:10:06 +0100

    Label documentation was broken

--- packages/pyxis-elm/src/Stories/Chapters/Label.elm
+++ packages/pyxis-elm/src/Stories/Chapters/Label.elm
@@ -17,38 +17,38 @@ Label component provides a label to be used within a form. It requires a _text_ 
 
 <component with-label="Label" />
 ```
-Import Components.Label as Label
+import Components.Label as Label
 
-    myLabel: Html msg
-    myLabel =
-        Label.create "Label"
-            |> Label.render
+myLabel: Html msg
+myLabel =
+    Label.create "Label"
+        |> Label.render
 ```
 
 ## For
 <component with-label="For" />
 ```
-Import Components.Label as Label
+import Components.Label as Label
 
-    myLabel: Html msg
-    myLabel =
-        Label.create "Label"
-            |> Label.withFor "input-id"
-            |> Label.render
+myLabel: Html msg
+myLabel =
+    Label.create "Label"
+        |> Label.withFor "input-id"
+        |> Label.render
 ```
 ## ID
 <component with-label="Id" />
 ```
-Import Components.Label as Label
+import Components.Label as Label
 
 {-| Note that setting an id to the Label also implies setting a data-test-id
 attribute with the same value of the id received.
 -}
-    myLabel: Html msg
-    myLabel =
-        Label.create "Label"
-            |> Label.withId "label-id"
-            |> Label.render
+myLabel: Html msg
+myLabel =
+    Label.create "Label"
+        |> Label.withId "label-id"
+        |> Label.render
 ```
 ## Variations
 Label can have an additional explanatory text or can be set with a smaller size.
@@ -56,25 +56,25 @@ Label can have an additional explanatory text or can be set with a smaller size.
 ### With an additional text
 <component with-label="Additional text" />
 ```
-Import Components.Label as Label
+import Components.Label as Label
 
-    myLabel: Html msg
-    myLabel =
-        Label.create "Main label"
-            |> Label.withSubText "This is an additional text"
-            |> Label.render
+myLabel: Html msg
+myLabel =
+    Label.create "Main label"
+        |> Label.withSubText "This is an additional text"
+        |> Label.render
 ```
 
 ### Size: small
 <component with-label="Small" />
 ```
-Import Components.Label as Label
+import Components.Label as Label
 
-    myLabel: Html msg
-    myLabel =
-        Label.create "Smaller label"
-            |> Label.withSizeSmall
-            |> Label.render
+myLabel: Html msg
+myLabel =
+    Label.create "Smaller label"
+        |> Label.withSizeSmall
+        |> Label.render
 ```
 ---
 ## Accessibility
