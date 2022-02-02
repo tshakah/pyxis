e7aaf67947861256c77f77585547bf728511ebe4
lauratrapani <59093016+lauratrapani@users.noreply.github.com>
2022-02-02 09:53:36 +0100

    [PRIMA-12283]: [Pyxis Scss] Badge implementation (#52)

---
+++ packages/pyxis-react/src/stories/components/Badge/Badge.tsx
@@ -0,0 +1,49 @@
+import {FC} from "react";
+import classNames from "classnames";
+import {pascalToKebab} from "commons/utils/string";
+import "./Badge.scss";
+
+const Badges = () => (
+  <>
+    <div className="wrapper">
+      <Badge />
+      <Badge variant="brand" />
+      <Badge variant="action" />
+      <Badge variant="success" />
+      <Badge variant="error" />
+      <Badge variant="alert" />
+      <Badge variant="neutralGradient" />
+      <Badge variant="brandGradient" />
+    </div>
+    <div className="wrapper bg-neutral-base">
+      <Badge alt />
+      <Badge variant="brand" alt />
+      <Badge variant="action" alt />
+      <Badge variant="success" alt />
+      <Badge variant="error" alt />
+      <Badge variant="alert" alt />
+      <Badge alt ghost />
+    </div>
+  </>
+);
+
+const setClasses = (alt:boolean , ghost:boolean, variant?:string):string => classNames(
+  "badge",
+  {
+    [`badge--${variant && pascalToKebab(variant)}`]: variant,
+    "badge--alt": alt && !ghost,
+    "badge--ghost": alt && ghost,
+  }
+);
+
+const Badge:FC<BadgeProps> = ({alt= false, variant , ghost = false}) => (
+  <span className={setClasses(alt, ghost, variant)}>Badge</span>
+);
+
+interface BadgeProps {
+  alt?: boolean;
+  variant?: "brand" | "action" | "error" | "success" | "alert" | "neutralGradient" | "brandGradient";
+  ghost?: boolean;
+}
+
+export default Badges;
