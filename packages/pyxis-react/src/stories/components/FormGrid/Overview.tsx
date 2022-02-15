68093b31bcdf270c53d15aa5384cce5147dd0d05
Emanuele Macri <emanuelemacri@me.com>
2022-02-15 17:17:33 +0100

    [PRIMA-12309]: [Pyxis SCSS] Form grid component (#69)

--- packages/pyxis-react/src/stories/components/FormGrid/Overview.tsx
+++ packages/pyxis-react/src/stories/components/FormGrid/Overview.tsx
@@ -1,125 +1,235 @@
 import React, {FC} from "react";
-import "./FormGrid.scss"
+import Button from "components/Button";
+import {
+  IconAlert,
+  IconCalendar,
+  IconChevronLeft,
+  IconExclamationCircle
+} from "components/Icon/Icons";
 
+// TODO: remove this implementation when grid will be implemented in pyxis-react
+// Non-exhaustive implementation, made for testing purposes only.
 const Overview: FC = () => (
-  <>
-    <div className="form-container">
-      <div className="form-row form-row--small">
-        <div className="form-item" />
+  <form className="form">
+    <fieldset>
+      <div className="form-grid form-grid--gap-large">
+        {/* Row con legend */}
+        <div className="form-grid__row form-grid__row--small">
+          <div className="form-grid__row__column">
+            <legend className="form-legend">
+              <span className="form-legend__title">Scegli il sinistro da denunciare</span>
+            </legend>
+          </div>
+        </div>
+        <div className="form-grid__row form-grid__row--large">
+          <div className="form-grid__row__column">
+            <div className="form-item">
+              <label className="form-label" id="pedoni-ciclisti-coinvolti">
+                Pedoni o ciclisti coinvolti
+              </label>
+              <div className="form-card-group" role="radiogroup" aria-labelledby="my-label-id">
+                <label className="form-card form-card--large">
+                  <span className="form-card__addon">
+                    <img src="static/media/placeholder.7d73ad27.svg" width="70" height="70" alt=""/>
+                  </span>
+                  <span className="form-card__content-wrapper">
+                    <span className="form-card__title">Veicoli</span>
+                  </span>
+                  <input type="radio" name="large" className="form-control__radio"/>
+                </label>
+                <label className="form-card form-card--large">
+                  <span className="form-card__addon">
+                    <img src="static/media/placeholder.7d73ad27.svg" width="70" height="70" alt=""/>
+                  </span>
+                  <span className="form-card__content-wrapper">
+                    <span className="form-card__title">Casa e Famiglia</span>
+                  </span>
+                  <input type="radio" name="large" className="form-control__radio"/>
+                </label>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="form-grid__row form-grid__row--small">
+          <div className="form-grid__row__column">
+            <div className="button-group button-group--center">
+              <Button size="large">Procedi</Button>
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
+    </fieldset>
+    <fieldset>
+      <div className="form-grid form-grid--gap-large">
+        {/* Row con messaggio */}
+        <div className="form-grid__row form-grid__row--large">
+          <div className="form-grid__row__column">
+            <div className="message message--alert message--with-background-color" role="status">
+              <div className="message__icon">
+                <IconAlert />
+              </div>
+              <div className="message__content-wrapper">
+                <div className="message__text">
+                  Per completare la denuncia di questo sinistro, è necessario rivolgersi anche alle compagnie degli altri veicoli coinvolti.
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
 
-    <div className="form-container form-container--left">
-      <div className="form-row form-row--small">
-        <div className="form-item" />
-      </div>
-    </div>
+        {/* Row con legend */}
+        <div className="form-grid__row form-grid__row--small">
+          <div className="form-grid__row__column">
+            <legend className="form-legend">
+              <span className="form-legend__title">Inserisci alcune informazioni di base</span>
+            </legend>
+          </div>
+        </div>
 
-    <div className="form-container form-container--right">
-      <div className="form-row form-row--small">
-        <div className="form-item" />
-      </div>
-    </div>
+        {/* Container con field */}
+        <div className="form-grid__row">
+          <div className="form-grid">
+            <div className="form-grid__row form-grid__row--small">
+              <div className="form-grid__row__column">
+                <div className="form-item">
+                  <label className="form-label" htmlFor="targa">
+                    Targa del veicolo assicurato con Prima
+                    <small className="form-label__sub">(Veicolo A)</small>
+                  </label>
+                  <div className="form-field">
+                  <label className="form-field__wrapper">
+                    <input
+                      type="text"
+                      className="form-field__text"
+                      id="targa"
+                      placeholder="AA123BC"
+                    />
+                  </label>
+                </div>
+                </div>
+              </div>
+            </div>
+            <div className="form-grid__row form-grid__row--small">
+              <div className="form-grid__row__column">
+                <div className="form-item">
+                  <label className="form-label" htmlFor="data-nascita">
+                    Data di nascita del proprietario
+                  </label>
+                  <div className="form-field form-field--with-prepend-icon">
+                  <label className="form-field__wrapper">
+                    <div className="form-field__addon">
+                      <IconCalendar />
+                    </div>
+                    <input type="date" className="form-field__date" id="data-nascita" placeholder="Date field"/>
+                  </label>
+                </div>
+                </div>
+              </div>
+            </div>
+            <div className="form-grid__row form-grid__row--small">
+              <div className="form-grid__row__column">
+                <div className="form-item">
+                  <label className="form-label" htmlFor="data-sinistro">
+                    Data del sinistro
+                  </label>
+                  <div className="form-field form-field--with-prepend-icon">
+                    <label className="form-field__wrapper">
+                      <div className="form-field__addon">
+                        <IconCalendar />
+                      </div>
+                      <input type="date" className="form-field__date" id="data-sinistro" placeholder="Date field"/>
+                    </label>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
 
-    <div className="form-container">
-      <div className="form-row form-row--small">
-        <div className="form-item" />
-      </div>
-      <div className="form-row form-row--small">
-        <div className="form-item" />
-        <div className="form-item" />
-      </div>
-      <div className="form-row form-row--small">
-        <div className="form-item" />
-        <div className="form-item" />
-        <div className="form-item" />
-      </div>
-      <div className="form-row form-row--small">
-        <div className="form-item form-item--2-col" />
-        <div className="form-item" />
-      </div>
-      <div className="form-row form-row--small">
-        <div className="form-item" />
-        <div className="form-item" />
-        <div className="form-item" />
-        <div className="form-item" />
-      </div>
-      <div className="form-row form-row--small">
-        <div className="form-item form-item--3-col" />
-        <div className="form-item" />
-      </div>
-    </div>
+        {/* Row con messaggio ghost */}
+        <div className="form-grid__row form-grid__row--small">
+          <div className="form-grid__row__column">
+            <div className="message message--ghost" role="status">
+              <div className="message__icon">
+                <IconExclamationCircle />
+              </div>
+              <div className="message__content-wrapper">
+                <div className="message__text">
+                  Se il veicolo non è assicurato con Prima, invia una mail a <a href="#" className="c-action-base">prima@prima.it</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
 
-    <div className="form-container">
-      <div className="form-row form-row--large">
-        <div className="form-item" />
+        {/* Row con button group*/}
+        <div className="form-grid__row form-grid__row--small">
+          <div className="form-grid__row__column">
+            <div className="button-group space-between">
+              <Button size="large" variant="ghost" icon={IconChevronLeft}>Indietro</Button>
+              <Button size="large">Procedi</Button>
+            </div>
+          </div>
+        </div>
       </div>
-      <div className="form-row form-row--large">
-        <div className="form-item" />
-        <div className="form-item" />
+    </fieldset>
+    <fieldset>
+      <div className="form-grid form-grid--gap-large">
+        {/* Row con legend */}
+        <div className="form-grid__row form-grid__row--small">
+          <div className="form-grid__row__column">
+            <legend className="form-legend">
+              <span className="form-legend__title">Inserisci i dettagli del sinistro</span>
+            </legend>
+          </div>
+        </div>
+
+        {/* Container con field */}
+        <div className="form-grid__row">
+          <div className="form-grid">
+            <div className="form-grid__row form-grid__row--small">
+              <div className="form-grid__row__column">
+                <div className="form-item">
+                  <label className="form-label" id="pedoni-ciclisti-coinvolti">
+                    Pedoni o ciclisti coinvolti
+                  </label>
+                  <div className="form-card-group form-card-group--row" role="radiogroup" aria-labelledby="pedoni-ciclisti-coinvolti">
+                    <label className="form-card">
+                    <span className="form-card__content-wrapper">
+                      <span className="form-card__text">Si</span>
+                    </span>
+                      <input type="radio" name="default" className="form-control__radio"/>
+                    </label>
+                    <label className="form-card">
+                      <span className="form-card__content-wrapper">
+                        <span className="form-card__text">No</span>
+                      </span>
+                      <input type="radio" name="default" className="form-control__radio"/>
+                    </label>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="form-grid__row form-grid__row--small">
+              <div className="form-grid__row__column">
+
+              </div>
+            </div>
+          </div>
+        </div>
+
+        {/* Row con button group*/}
+        <div className="form-grid__row form-grid__row--small">
+          <div className="form-grid__row__column">
+            <div className="button-group space-between">
+              <Button size="large" variant="ghost" icon={IconChevronLeft}>Indietro</Button>
+              <Button size="large">Procedi</Button>
+            </div>
+          </div>
+        </div>
       </div>
-      <div className="form-row form-row--large">
-        <div className="form-item" />
-        <div className="form-item" />
-        <div className="form-item" />
-      </div>
-      <div className="form-row form-row--large">
-        <div className="form-item form-item--2-col" />
-        <div className="form-item" />
-      </div>
-      <div className="form-row form-row--large">
-        <div className="form-item" />
-        <div className="form-item" />
-        <div className="form-item" />
-        <div className="form-item" />
-      </div>
-      <div className="form-row form-row--large">
-        <div className="form-item form-item--3-col" />
-        <div className="form-item" />
-      </div>
-      <div className="form-row form-row--large">
-        <div className="form-item" />
-        <div className="form-item" />
-        <div className="form-item" />
-        <div className="form-item" />
-        <div className="form-item" />
-      </div>
-      <div className="form-row form-row--large">
-        <div className="form-item form-item--3-col" />
-        <div className="form-item form-item--2-col" />
-      </div>
-      <div className="form-row form-row--large">
-        <div className="form-item form-item--4-col" />
-        <div className="form-item" />
-      </div>
-      <div className="form-row form-row--large">
-        <div className="form-item" />
-        <div className="form-item" />
-        <div className="form-item" />
-        <div className="form-item" />
-        <div className="form-item" />
-        <div className="form-item" />
-      </div>
-      <div className="form-row form-row--large">
-        <div className="form-item form-item--3-col" />
-        <div className="form-item form-item--2-col" />
-        <div className="form-item" />
-      </div>
-      <div className="form-row form-row--large">
-        <div className="form-item form-item--4-col" />
-        <div className="form-item form-item--2-col" />
-      </div>
-      <div className="form-row form-row--large">
-        <div className="form-item form-item--5-col" />
-        <div className="form-item" />
-      </div>
-      <div className="form-row form-row--large">
-        <div className="form-item form-item--4-col" />
-        <div className="form-item--spacing " />
-        <div className="form-item " />
-      </div>
-    </div>
-  </>
+    </fieldset>
+  </form>
 )
 
 export default Overview;
---
+++ packages/pyxis-react/src/stories/components/Legend/Legend.tsx
@@ -0,0 +1,73 @@
+import React, {FC} from "react";
+import {IconCar} from "components/Icon/Icons";
+import placeholder from "stories/assets/placeholder.svg";
+import CheckboxCard from "../CheckboxCard/CheckboxCard";
+
+// TODO: remove this implementation when legend will be implemented in pyxis-react
+// Non-exhaustive implementation, made for testing purposes only.
+export const Legends = () =>
+  <form className="form">
+    <fieldset>
+      <div className="form-grid">
+        <div className="form-grid__row">
+          <div className="form-grid__row__column">
+            <Legend withIcon/>
+          </div>
+        </div>
+      </div>
+    </fieldset>
+    <fieldset>
+      <div className="form-grid">
+        <div className="form-grid__row">
+          <div className="form-grid__row__column">
+            <Legend withImage/>
+          </div>
+        </div>
+      </div>
+    </fieldset>
+    <fieldset>
+      <div className="form-grid">
+        <div className="form-grid__row">
+          <div className="form-grid__row__column">
+            <Legend/>
+          </div>
+        </div>
+      </div>
+    </fieldset>
+    <fieldset>
+      <div className="form-grid">
+        <div className="form-grid__row">
+          <div className="form-grid__row__column">
+            <Legend alignLeft/>
+          </div>
+        </div>
+      </div>
+    </fieldset>
+  </form>
+
+const Legend:FC<LegendProps> =
+  ({
+     withDescription = true,
+     withIcon = false,
+     withImage = false,
+     alignLeft = false
+  }) =>
+  <legend className={`form-legend ${alignLeft ? "form-legend--align-left" : ""}`}>
+    {(withIcon || withImage) &&
+      <span className="form-legend__addon">
+        {withIcon && <IconCar boxed/>}
+        {withImage && <img src={placeholder} width={56} height={56} alt=""/>}
+      </span>
+    }
+    <span className="form-legend__title">Legend title</span>
+    {withDescription && <span className="form-legend__text">Legend description</span>}
+  </legend>
+
+interface LegendProps {
+  alignLeft?: boolean;
+  withDescription?: boolean;
+  withIcon?: boolean;
+  withImage?: boolean;
+}
+
+export default Legend;
