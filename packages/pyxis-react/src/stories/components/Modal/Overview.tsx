35487e182c3e6aaca890bb423ded64d0be42b9a1
Emanuele Macri <emanuelemacri@me.com>
2022-04-14 17:39:49 +0200

    [DS-158]: [Pyxis Scss] Improve the modal component (#144)

--- packages/pyxis-react/src/stories/components/Modal/Overview.tsx
+++ packages/pyxis-react/src/stories/components/Modal/Overview.tsx
@@ -1,51 +1,57 @@
-import React, {FC, useState} from "react";
+import React, {FC, useEffect, useState} from "react";
 import classNames from "classnames";
-import Badge from "stories/components/Badge/Badge";
 import Button from "components/Button";
-import {IconClose} from "components/Icon/Icons";
+import {IconCar, IconClose} from "components/Icon/Icons";
+import Badge from "../Badge/Badge";
 
-const getClasses = (centered: boolean): string => classNames(
-  "modal",
-  "modal--small",
+const getClasses = (show: boolean): string => classNames(
+  "modal-backdrop",
   {
-    ["modal--center"]: centered,
+    ["modal-backdrop--show"]: show,
   },
 );
 
 const Overview: FC = () => {
   const [isOpened, setIsOpened] = useState(false);
-  const [isCenter, setIsCenter] = useState(false);
+
+  useEffect(() => {
+    isOpened
+      ? document.documentElement.classList.add('scroll-locked')
+      : document.documentElement.classList.remove('scroll-locked');
+  }, [isOpened])
 
   return (
     <>
-      <div className="modal-backdrop">
-        <div className={getClasses(isCenter)}>
+      <Button size="large" onClick={() => setIsOpened(true)} style={{marginBottom: "200vh"}}>Open Modal</Button>
+      <div
+        aria-describedby='modal-description'
+        aria-hidden={!isOpened}
+        aria-labelledby='modal-label'
+        className={getClasses(isOpened)}
+        role="dialog"
+      >
+        <div id="modal-description" className="screen-reader-only">Accessibility description</div>
+        <div className="modal-close" onClick={() => setIsOpened(false)}/>
+        <div className="modal modal--large">
           <header className="modal__header">
             <div className="modal__header__wrapper">
-              <Badge variant="brand">Badge</Badge>
-              <h3 className="modal__header__title">Come recuperare i MQ commerciali</h3>
+              <div className="modal__header__badge">
+                <Badge variant="brand">Badge</Badge>
+              </div>
+              <IconCar className="c-brand-base" size="l" />
+              <h3 className="modal__header__title" id="modal-label">Come recuperare i MQ commerciali</h3>
             </div>
-            <Button icon={IconClose} iconPlacement="only" variant="tertiary" size="medium">Close</Button>
+            <Button icon={IconClose} iconPlacement="only" variant="tertiary" size="medium" onClick={() => setIsOpened(false)}>Close</Button>
           </header>
           <div className="modal__content">
-            <div className="button-row">
-              <Button onClick={() => setIsOpened(!isOpened)}>
-                {isOpened ? "hide content" : "show content"}
-              </Button>
-              <Button onClick={() => setIsCenter(!isCenter)}>
-                {isCenter ? "on top" : "on center"}
-              </Button>
-            </div>
-            {isOpened &&
-              <div className="modal-custom-content">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ultrices libero ac semper cursus. Integer scelerisque mi at blandit vestibulum. Vivamus nec nibh id lacus lacinia facilisis vel nec felis. Duis rhoncus rutrum volutpat. Quisque at pulvinar enim. Vestibulum ut posuere erat. Quisque cursus ut odio vel faucibus. Duis semper venenatis finibus. Morbi iaculis ligula at justo lobortis vulputate. Duis vestibulum neque at neque fringilla malesuada. Nulla vitae nunc sed lectus varius facilisis in eu nisi. Aliquam erat volutpat. Phasellus sapien elit, suscipit id eleifend sed, posuere ac nulla. Duis volutpat, mauris sit amet tincidunt ornare, nunc erat semper ex, quis rhoncus eros nulla et metus. Vestibulum eu egestas felis, quis porta lectus.
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ultrices libero ac semper cursus. Integer scelerisque mi at blandit vestibulum. Vivamus nec nibh id lacus lacinia facilisis vel nec felis. Duis rhoncus rutrum volutpat. Quisque at pulvinar enim. Vestibulum ut posuere erat. Quisque cursus ut odio vel faucibus. Duis semper venenatis finibus. Morbi iaculis ligula at justo lobortis vulputate. Duis vestibulum neque at neque fringilla malesuada. Nulla vitae nunc sed lectus varius facilisis in eu nisi. Aliquam erat volutpat. Phasellus sapien elit, suscipit id eleifend sed, posuere ac nulla. Duis volutpat, mauris sit amet tincidunt ornare, nunc erat semper ex, quis rhoncus eros nulla et metus. Vestibulum eu egestas felis, quis porta lectus.
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ultrices libero ac semper cursus. Integer scelerisque mi at blandit vestibulum. Vivamus nec nibh id lacus lacinia facilisis vel nec felis. Duis rhoncus rutrum volutpat. Quisque at pulvinar enim. Vestibulum ut posuere erat. Quisque cursus ut odio vel faucibus. Duis semper venenatis finibus. Morbi iaculis ligula at justo lobortis vulputate. Duis vestibulum neque at neque fringilla malesuada. Nulla vitae nunc sed lectus varius facilisis in eu nisi. Aliquam erat volutpat. Phasellus sapien elit, suscipit id eleifend sed, posuere ac nulla. Duis volutpat, mauris sit amet tincidunt ornare, nunc erat semper ex, quis rhoncus eros nulla et metus. Vestibulum eu egestas felis, quis porta lectus.
-              </div>
-            }
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ultrices libero ac semper cursus. Integer scelerisque mi at blandit vestibulum. Vivamus nec nibh id lacus lacinia facilisis vel nec felis. Duis rhoncus rutrum volutpat. Quisque at pulvinar enim. Vestibulum ut posuere erat. Quisque cursus ut odio vel faucibus. Duis semper venenatis finibus. Morbi iaculis ligula at justo lobortis vulputate. Duis vestibulum neque at neque fringilla malesuada. Nulla vitae nunc sed lectus varius facilisis in eu nisi. Aliquam erat volutpat. Phasellus sapien elit, suscipit id eleifend sed, posuere ac nulla. Duis volutpat, mauris sit amet tincidunt ornare, nunc erat semper ex, quis rhoncus eros nulla et metus. Vestibulum eu egestas felis, quis porta lectus.
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ultrices libero ac semper cursus. Integer scelerisque mi at blandit vestibulum. Vivamus nec nibh id lacus lacinia facilisis vel nec felis. Duis rhoncus rutrum volutpat. Quisque at pulvinar enim. Vestibulum ut posuere erat. Quisque cursus ut odio vel faucibus. Duis semper venenatis finibus. Morbi iaculis ligula at justo lobortis vulputate. Duis vestibulum neque at neque fringilla malesuada. Nulla vitae nunc sed lectus varius facilisis in eu nisi. Aliquam erat volutpat. Phasellus sapien elit, suscipit id eleifend sed, posuere ac nulla. Duis volutpat, mauris sit amet tincidunt ornare, nunc erat semper ex, quis rhoncus eros nulla et metus. Vestibulum eu egestas felis, quis porta lectus.
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ultrices libero ac semper cursus. Integer scelerisque mi at blandit vestibulum. Vivamus nec nibh id lacus lacinia facilisis vel nec felis. Duis rhoncus rutrum volutpat. Quisque at pulvinar enim. Vestibulum ut posuere erat. Quisque cursus ut odio vel faucibus. Duis semper venenatis finibus. Morbi iaculis ligula at justo lobortis vulputate. Duis vestibulum neque at neque fringilla malesuada. Nulla vitae nunc sed lectus varius facilisis in eu nisi. Aliquam erat volutpat. Phasellus sapien elit, suscipit id eleifend sed, posuere ac nulla. Duis volutpat, mauris sit amet tincidunt ornare, nunc erat semper ex, quis rhoncus eros nulla et metus. Vestibulum eu egestas felis, quis porta lectus.
           </div>
           <footer className="modal__footer">
-            <Button>Button footer</Button>
+            <div className="modal__footer__buttons modal__footer__buttons--full-width">
+              <Button size="large">Button footer</Button>
+            </div>
           </footer>
         </div>
       </div>
