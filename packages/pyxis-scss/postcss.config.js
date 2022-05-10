6c2138b3c81f8103feee89506601d06e80fccdb8
Laura Trapani <laura.trapani@prima.it>
2022-05-10 14:49:32 +0200

    fix button

--- packages/pyxis-scss/postcss.config.js
+++ packages/pyxis-scss/postcss.config.js
@@ -2,6 +2,7 @@ module.exports = {
   plugins: [
     require('autoprefixer'),
     require('postcss-selector-not').default,
+    require('postcss-gap')({ method: 'duplicate' }),
     require('cssnano'),
   ]
 }
